64eaa77fc7decf41f74f4d4eaa3f3976b6278a4a
Conrad Kleinespel <conradk@conradk.com>
2020-01-20 14:12:56 +0100

    simplifies heroku install

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,18 @@
 FROM node:latest
 
-RUN npm install -g heroku
-WORKDIR /home/node/heroku
-
 COPY docker-entrypoint.sh /usr/bin/docker-entrypoint.sh
 RUN chmod o+rx /usr/bin/docker-entrypoint.sh
 ENTRYPOINT ["/usr/bin/docker-entrypoint.sh"]
 
 ADD . /home/node/heroku
-RUN npm install
+WORKDIR /home/node/heroku
 RUN chown -R node:node /home/node
+
 USER node
+RUN mkdir /home/node/.npm-packages
+RUN npm config set prefix /home/node/.npm-packages
+ENV NPM_PACKAGES /home/node/.npm-packages
+ENV PATH $NPM_PACKAGES/bin:$PATH
+RUN npm install -g heroku
+RUN heroku plugins:install heroku-repo
+RUN npm install
--- purge-cache.js
+++ purge-cache.js
@@ -6,20 +6,15 @@ const getStartOfWeek = () => moment().startOf('week').format('DD/MM/YYYY')
 let lastDayDone = null
 
 const purgeCacheOnceAWeek = () => {
-    console.log(`------------------------------------------------------------`);
-    console.log(`Checking if cache has been purged on the week of: ${getStartOfWeek()}`);
-    console.log(`------------------------------------------------------------`);
-
     const currentStartOfWeek = getStartOfWeek()
     if (lastDayDone === currentStartOfWeek) {
-        console.log('------------------------');
-        console.log('Check back in an hour...');
-        console.log('------------------------');
         setTimeout(purgeCacheOnceAWeek, 1000 * 60 * 60);
         return;
     }
 
     lastDayDone = currentStartOfWeek
+
+    console.log('Purging cache for ' + currentStartOfWeek);
 
     const child = spawn('heroku', ['repo:purge_cache', '-a', process.env.HEROKU_APP]);
     
@@ -27,21 +22,8 @@ const purgeCacheOnceAWeek = () => {
     child.stdout.pipe(process.stdout);
 
     child.on('close', () => {
-        console.log('------------------------');
-        console.log('Check back in an hour...');
-        console.log('------------------------');
         setTimeout(purgeCacheOnceAWeek, 1000 * 60 * 60);
     });
 }
 
-const pluginInstallChild = spawn('heroku', ['plugins:install', 'heroku-repo']);
-
-pluginInstallChild.stderr.pipe(process.stderr);
-pluginInstallChild.stdout.pipe(process.stdout);
-pluginInstallChild.on('close', (code) => {
-    if (code !== 0) {
-        return;
-    }
-
-    purgeCacheOnceAWeek();
-})
+purgeCacheOnceAWeek();
